989c47a713f1c5ac816fc2cbb3a89b6f9c387775
zhydani <zhydani@gmail.com>
2025-01-08 19:00:41 -0300

    ajuste da intro

--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -1,9 +1,12 @@
+import { Box } from "@mui/material";
 import React from "react";
+import IntroSection from "./IntroSection";
 
 export default function Content() {
 
   return (
-    <>
-    </>
+    <Box>
+      <IntroSection />
+    </Box>
   );
 }
---
+++ src/app/components/IntroSection.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Box, Container, Typography } from "@mui/material";
+import Image from "next/image";
+
+const IntroSection = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        flexDirection: { xs: "column", md: "row" },
+        backgroundColor: "#f9eefe",
+        padding: "3rem 2rem",
+        minHeight: "80vh",
+      }}
+    >
+      <Container
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          flexDirection: { xs: "column", md: "row" },
+        }}
+      >
+        <Box
+          sx={{
+            textAlign: { xs: "center", md: "left" },
+            maxWidth: "600px",
+          }}
+        >
+          <Typography
+            variant="h2"
+            sx={{
+              fontWeight: "bold",
+              fontSize: { xs: "2rem", md: "3.5rem" },
+              color: "#000",
+            }}
+          >
+            Olá, sou Zhydani Reis
+          </Typography>
+          <Typography
+            variant="h6"
+            sx={{
+              marginTop: "1rem",
+              fontSize: { xs: "1rem", md: "1.5rem" },
+              color: "#444",
+            }}
+          >
+            Sou Desenvolvedora Fullstack
+            <br />
+            & Web Desginer
+          </Typography>
+        </Box>
+
+        <Box
+          sx={{
+            borderRadius: "50%",
+            overflow: "hidden",
+            marginTop: { xs: "2rem", md: "0" },
+            width: { xs: "200px", md: "300px" },
+            height: { xs: "200px", md: "300px" },
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            src="/perfil.jpg"
+            alt="Zhydani Reis"
+            layout="responsive"
+            width={300}
+            height={300}
+          />
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default IntroSection;
