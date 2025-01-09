5004d553556be0df29b9b1f2ab701f9578db2596
zhydani <zhydani@gmail.com>
2025-01-09 11:55:08 -0300

    about section

---
+++ src/app/components/AboutSection.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, Container } from "@mui/material";
+
+const AboutSection = () => {
+  return (
+    <Box>
+      <Container>
+        
+      </Container>
+    </Box>
+  );
+};
+
+export default AboutSection;
--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -1,12 +1,14 @@
 import { Box } from "@mui/material";
 import React from "react";
 import IntroSection from "./IntroSection";
+import AboutSection from "./AboutSection";
 
 export default function Content() {
 
   return (
     <Box>
       <IntroSection />
+      <AboutSection />
     </Box>
   );
 }
