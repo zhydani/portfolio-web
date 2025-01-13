56577fbe2592e617c27569c73ba7553140ceaa11
zhydani <zhydani@gmail.com>
2025-01-13 15:22:54 -0300

    ajustando breadcru,bs

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -2,10 +2,12 @@
 
 import { Box } from "@mui/material";
 import Content from "./components/Content";
+import { BreadcrumbsSection } from "@/components/ui";
 
 export default function SobrePage() {
   return (
     <Box>
+        <BreadcrumbsSection pageName="Sobre" />
         <Content />
     </Box>
   );
