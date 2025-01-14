f3acb876e24306147757f05bbdc8d1c6fface1c5
zhydani <zhydani@gmail.com>
2025-01-14 09:43:46 -0300

    ajuste versao mobile

--- src/app/components/IntroSection.tsx
+++ src/app/components/IntroSection.tsx
@@ -9,11 +9,12 @@ const IntroSection = () => {
       sx={{
         display: "flex",
         alignItems: "center",
-        justifyContent: "space-between",
-        flexDirection: { xs: "column", md: "row" },
+        justifyContent: "center",
+        flexDirection: "column",
         backgroundColor: "#f9eefe",
-        padding: "3rem 2rem",
+        padding: { xs: "2rem 1rem", md: "3rem 2rem" },
         minHeight: "80vh",
+        textAlign: { xs: "center", md: "left" },
       }}
     >
       <Container
@@ -22,42 +23,45 @@ const IntroSection = () => {
           alignItems: "center",
           justifyContent: "space-between",
           flexDirection: { xs: "column", md: "row" },
+          maxWidth: "lg",
         }}
       >
         <Box
           sx={{
-            textAlign: { xs: "center", md: "left" },
-            maxWidth: "600px",
+            maxWidth: { xs: "100%", md: "600px" },
+            mb: { xs: 3, md: 0 },
           }}
         >
           <Typography
             variant="h2"
             sx={{
               fontWeight: "bold",
-              fontSize: { xs: "2rem", md: "3.5rem" },
+              fontSize: { xs: "1.8rem", sm: "2.5rem", md: "3.5rem" },
               color: "#000",
+              lineHeight: 1.2,
             }}
           >
             <Typewriter
               words={["Olá, sou Zhydani Reis"]}
-              loop={1} 
+              loop={1}
               cursor
               cursorStyle="|"
-              typeSpeed={100} 
-              deleteSpeed={50} 
+              typeSpeed={100}
+              deleteSpeed={50}
             />
           </Typography>
           <Typography
             variant="h6"
             sx={{
               marginTop: "1rem",
-              fontSize: { xs: "1rem", md: "1.5rem" },
+              fontSize: { xs: "0.9rem", sm: "1rem", md: "1.5rem" },
               color: "#444",
+              lineHeight: 1.6,
             }}
           >
             Ajudo empresas e pessoas a criar e melhorar 
             <br />
-            seus sites, aplicativos e designs para a web
+            seus sites, aplicativos e designs para a web.
           </Typography>
         </Box>
 
@@ -65,12 +69,12 @@ const IntroSection = () => {
           sx={{
             borderRadius: "50%",
             overflow: "hidden",
-            marginTop: { xs: "2rem", md: "0" },
-            width: { xs: "200px", md: "300px" },
-            height: { xs: "200px", md: "300px" },
+            width: { xs: "150px", sm: "200px", md: "300px" },
+            height: { xs: "150px", sm: "200px", md: "300px" },
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
+            boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
           }}
         >
           <Image
@@ -78,6 +82,11 @@ const IntroSection = () => {
             alt="Zhydani Reis"
             width={300}
             height={300}
+            style={{
+              objectFit: "contain",
+              width: "100%", 
+              height: "100%",
+            }}
           />
         </Box>
       </Container>
